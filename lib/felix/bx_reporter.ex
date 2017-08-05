6610a7b36f4eb44be2a3684e5b5e0adcab4af7b5
Teerawat Lamanchart <ton@omise.co>
2017-08-05 16:08:11 +0700

    Wrap notify function with Task module
    To prevent the whole app crash.

--- lib/felix/bx_reporter.ex
+++ lib/felix/bx_reporter.ex
@@ -21,27 +21,29 @@ defmodule Felix.BXReporter do
   end
 
   defp notify do
-    pairings =
-      @url
-      |> HTTPoison.get!()
-      |> Map.get(:body)
-      |> Poison.decode!()
+    Task.start fn ->
+      pairings =
+        @url
+        |> HTTPoison.get!()
+        |> Map.get(:body)
+        |> Poison.decode!()
 
-    omg = Map.get(pairings, "26")
-    eth = Map.get(pairings, "21")
-    xrp = Map.get(pairings, "25")
+      omg = Map.get(pairings, "26")
+      eth = Map.get(pairings, "21")
+      xrp = Map.get(pairings, "25")
 
-    message = Enum.reduce [omg, eth, xrp], "", fn(info, acc) ->
-      acc <> """
-      ```
-      ----- #{info["secondary_currency"]} -----
-      Last price: #{info["last_price"]}
-      Change: #{info["change"]}
-      ```
-      """
+      message = Enum.reduce [omg, eth, xrp], "", fn(info, acc) ->
+        acc <> """
+        ```
+        ----- #{info["secondary_currency"]} -----
+        Last price: #{info["last_price"]}
+        Change: #{info["change"]}
+        ```
+        """
+      end
+
+      Slack.Web.Chat.post_message("#omg", message)
     end
-
-    Slack.Web.Chat.post_message("#omg", message)
   end
 
   defp schedule_work do
